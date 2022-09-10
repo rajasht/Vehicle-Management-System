366fc15ca4eb66bcc90ebb46893b93b5165824ea
rajasht <Raja@shorthillstech.com>
2022-09-10 22:34:25 +0530

    Added UserTest Cases and Modified Car Test Cases alongwith phpunit.xml for sqlite based in memory testing

--- tests/Unit/CarTest.php
+++ tests/Unit/CarTest.php
@@ -4,9 +4,13 @@ namespace Tests\Unit;
 
 use App\Models\Car;
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\Response;
 
 class CarTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * A basic unit test example.
      *
@@ -40,29 +44,31 @@ class CarTest extends TestCase
             "wheel_base_mm" => 2500
         ]);
         $car2 = Car::make([
-            "car_name" => "Verna",
-            "price_rs" => 1245000,
-            "brand" => "Hyundai",
-            "model" => "SX",
-            "model_year" => 2022,
-            "colors_available" => 6,
-            "wheel_count" => 4,
-            "fuel_type" => "Diesel",
-            "record_status" => 1,
-            "first_production_year" => 2006,
+            "car_name" => "Dzire",
+            "brand" => "Maruti Suzuki",
             "transmission" => "Manual",
-            "engine_displacement_cc" => 1493,
-            "seating_capacity" => 5,
-            "fuel_tank_capacity_litres" => 45,
-            "body_type" => "Sedan",
-            "mileage_kmpl" => 25,
-            "rpm" => 4000,
-            "max_power_bhp" => 113,
-            "max_torque_nm" => 250,
-            "length_mm" => 4440,
-            "width_mm" => 1729,
-            "height_mm" => 1475,
-            "wheel_base_mm" => 2600
+            "model"=> "VXI",
+            "model_year"=> 2022,
+            "seating_capacity"=> 5,
+            "fuel_type"=> "Petrol",
+            "fuel_tank_capacity_litres"=> 37,
+            "mileage_kmpl"=> 23,
+            "engine_displacement_cc"=> 1197,
+            "body_type"=> "Sedan",
+            "wheel_base_mm"=> 2450,
+            "rpm"=> 6000,
+            "max_power_bhp"=> 89,
+            "max_torque_nm"=> 113,
+            "length_mm"=> 3995,
+            "width_mm"=> 1735,
+            "height_mm"=> 1515,
+            "vin"=> "4ABCD56EFGH789012",
+            "engine_number"=> "45ABC67890",
+            "price_rs"=> 728000,
+            "colors_available"=> 6,
+            "wheel_count"=> 4,
+            "record_status"=> 1,
+            "user_id"=> 2
         ]);
 
         $this->assertTrue($car1->car_name != $car2->car_name);
@@ -71,29 +77,31 @@ class CarTest extends TestCase
     public function test_delete_car()
     {
         $car = Car::make([
-            "car_name" => "Venue",
-            "price_rs" => 1070000,
-            "brand" => "Hyundai",
-            "model" => "SX",
-            "model_year" => 2022,
-            "colors_available" => 7,
-            "wheel_count" => 4,
-            "fuel_type" => "Petrol",
-            "record_status" => 1,
-            "first_production_year" => 2019,
+            "car_name" => "Dzire",
+            "brand" => "Maruti Suzuki",
             "transmission" => "Manual",
-            "engine_displacement_cc" => 1197,
-            "seating_capacity" => 5,
-            "fuel_tank_capacity_litres" => 45,
-            "body_type" => "SUV",
-            "mileage_kmpl" => 16,
-            "rpm" => 6000,
-            "max_power_bhp" => 82,
-            "max_torque_nm" => 113.8,
-            "length_mm" => 3995,
-            "width_mm" => 1770,
-            "height_mm" => 1617,
-            "wheel_base_mm" => 2500
+            "model"=> "VXI",
+            "model_year"=> 2022,
+            "seating_capacity"=> 5,
+            "fuel_type"=> "Petrol",
+            "fuel_tank_capacity_litres"=> 37,
+            "mileage_kmpl"=> 23,
+            "engine_displacement_cc"=> 1197,
+            "body_type"=> "Sedan",
+            "wheel_base_mm"=> 2450,
+            "rpm"=> 6000,
+            "max_power_bhp"=> 89,
+            "max_torque_nm"=> 113,
+            "length_mm"=> 3995,
+            "width_mm"=> 1735,
+            "height_mm"=> 1515,
+            "vin"=> "4ABCD56EFGH789012",
+            "engine_number"=> "45ABC67890",
+            "price_rs"=> 728000,
+            "colors_available"=> 6,
+            "wheel_count"=> 4,
+            "record_status"=> 1,
+            "user_id"=> 2
         ]);
         $car = Car::first();
         if ($car) {
@@ -104,41 +112,69 @@ class CarTest extends TestCase
 
     public function test_stores_new_car()
     {
-        $response = $this->post('/api/cars', [
-            "car_name" => "Verna",
-            "price_rs" => 1245000,
-            "brand" => "Hyundai",
-            "model" => "SX",
-            "model_year" => 2022,
-            "colors_available" => 6,
-            "wheel_count" => 4,
-            "fuel_type" => "Diesel",
-            "record_status" => 1,
-            "first_production_year" => 2006,
-            "transmission" => "Manual",
-            "engine_displacement_cc" => 1493,
-            "seating_capacity" => 5,
-            "fuel_tank_capacity_litres" => 45,
-            "body_type" => "Sedan",
-            "mileage_kmpl" => 25,
-            "rpm" => 4000,
-            "max_power_bhp" => 113,
-            "max_torque_nm" => 250,
-            "length_mm" => 4440,
-            "width_mm" => 1729,
-            "height_mm" => 1475,
-            "wheel_base_mm" => 2600
-        ]);
-        $response->assertStatus(201);
-    }
 
-    public function test_database()
-    {
-        $this->assertDatabaseHas('cars', [
-            'car_name' => 'Verna'
-        ]);
-        $this->assertDatabaseMissing('cars', [
-            'car_name' => 'Bike'
-        ]);
+        $payload = [
+            "car_name" => "Dzire",
+            "brand" => "Maruti Suzuki",
+            "transmission" => "Manual",
+            "model"=> "VXI",
+            "model_year"=> 2022,
+            "seating_capacity"=> 5,
+            "fuel_type"=> "Petrol",
+            "fuel_tank_capacity_litres"=> 37,
+            "mileage_kmpl"=> 23,
+            "engine_displacement_cc"=> 1197,
+            "body_type"=> "Sedan",
+            "wheel_base_mm"=> 2450,
+            "rpm"=> 6000,
+            "max_power_bhp"=> 89,
+            "max_torque_nm"=> 113,
+            "length_mm"=> 3995,
+            "width_mm"=> 1735,
+            "height_mm"=> 1515,
+            "vin"=> "4ABCD56EFGH789012",
+            "engine_number"=> "45ABC67890",
+            "price_rs"=> 728000,
+            "colors_available"=> 6,
+            "wheel_count"=> 4,
+            "record_status"=> 1,
+            "user_id"=> 2
+        ];
+        $this->json('post', 'api/cars', $payload)
+             ->assertStatus(Response::HTTP_CREATED)
+             ->assertJsonStructure(
+                 [
+                     'data' => [
+                         'id',
+                         'car_name',
+                        'brand',
+                        'transmission',
+                        'model',
+                        'model_year',
+                        'seating_capacity',
+                        'fuel_type',
+                        'fuel_tank_capacity_litres',
+                        'mileage_kmpl',
+                        'engine_displacement_cc',
+                        'body_type',
+                        'wheel_base_mm',
+                        'rpm',
+                        'max_power_bhp',
+                        'max_torque_nm',
+                        'length_mm',
+                        'width_mm',
+                        'height_mm',
+                        'vin',
+                        'engine_number',
+                        'price_rs',
+                        'colors_available',
+                        'wheel_count',
+                        'record_status',
+                        'user_id',
+                         ]
+                ]
+             );
+        $this->assertDatabaseHas('cars', $payload);
+        $this->assertDatabaseMissing('cars', ["vin" => "4ABCD56EFGH789000"]);
     }
 }
